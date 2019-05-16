a08b64a1eebf538f9282d4311850a794919fa410
Milan Dzenovljanovic <md@innovation.rocks>
2019-05-16 15:20:11 +0200

    add new param to tweet options interface

--- projects/ngx-tweet-line/src/lib/ngx-tweet-options.interface.ts
+++ projects/ngx-tweet-line/src/lib/ngx-tweet-options.interface.ts
@@ -1,17 +1,18 @@
 export default interface NgxTweetOptions {
-    /**
-     * "show" or "hidden"
-     */
-    cards?: string;
-    /**
-     * "show" or "none"
-     */
-    conversation?: string;
-    linkColor?: string;
-    /**
-     * "dark" or "light"
-     */
-    theme?: string;
-    url?: string;
-    linkTitle?: string;
+  /**
+   * "show" or "hidden"
+   */
+  cards?: string;
+  /**
+   * "show" or "none"
+   */
+  conversation?: string;
+  linkColor?: string;
+  /**
+   * "dark" or "light"
+   */
+  theme?: string;
+  url?: string;
+  linkTitle?: string;
+  width?: number;
 }
