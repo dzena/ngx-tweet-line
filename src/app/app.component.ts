f3f6d1cabf84861f0d6be7ceadf1332f0e902279
Milan Dzenovljanovic <md@innovation.rocks>
2019-05-17 09:38:42 +0200

    testing loading in the host app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: [ './app.component.css' ]
+} )
 export class AppComponent {
   title = 'ngx-tweet-line';
   color = '#000000';
   theme = 'light';
   theme1 = 'light';
+
+  public isLoadingTweet;
+  public isLoadingTimeline;
+
+  constructor( private cdRef: ChangeDetectorRef ) {}
+
+  loadTweet( loading: boolean ) {
+    this.isLoadingTweet = loading;
+    this.cdRef.detectChanges();
+  }
+
+  loadTimeline( loading: boolean ) {
+    this.isLoadingTimeline = loading;
+    this.cdRef.detectChanges();
+  }
 }
