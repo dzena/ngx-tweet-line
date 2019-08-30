fe1194b6b76a7b2d8d44c9836e1ada565c138f48
Ciprian Buta <cb@innovation.rocks>
2019-08-30 16:20:43 +0200

    export to public

--- projects/ngx-tweet-line/src/public_api.ts
+++ projects/ngx-tweet-line/src/public_api.ts
@@ -5,4 +5,5 @@
 export * from './lib/ngx-tweet-line.service';
 export * from './lib/ngx-tweet.component';
 export * from './lib/ngx-twitter-timeline.component';
+export * from './lib/ngx-follow-btn.component';
 export * from './lib/ngx-tweet-line.module';
