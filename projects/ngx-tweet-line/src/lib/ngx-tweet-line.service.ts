390b2e2732ad8506603c100e0b0fa8c8b0db95af
Milan Dzenovljanovic <md@innovation.rocks>
2020-10-18 14:42:49 +0200

    chore - refactor ngx-tweet-line.service.ts to use angular api to load 3rd party scripts, improve code expressiveness and readability

--- projects/ngx-tweet-line/src/lib/ngx-tweet-line.service.ts
+++ projects/ngx-tweet-line/src/lib/ngx-tweet-line.service.ts
@@ -1,50 +1,70 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Inject, Injectable } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
 
+// TODO write type definitions for Twitter API
+
+/**
+ * This class takes care of loading the twitter script
+ */
 @Injectable( {
   providedIn: 'root'
 } )
 export class NgxTweetLineService {
+  private readonly TWITTER_OBJECT = 'twttr';
   private readonly TWITTER_SCRIPT_ID = 'twitter-wjs';
   private readonly TWITTER_WIDGET_URL = 'https://platform.twitter.com/widgets.js';
 
-  constructor() { }
+  constructor( @Inject( DOCUMENT ) private readonly _document: Document ) {}
 
   loadScript(): Observable<any> {
-    return Observable.create( observer => {
+    return new Observable( ( observer: Observer<any> ) => {
 
-      this.startScriptLoad();
+      this._startScriptLoad();
 
-      window[ 'twttr' ].ready( ( twttr ) => {
-        observer.next( twttr );
-        observer.complete();
-      } );
+      this._document.defaultView[ this.TWITTER_OBJECT ].ready( this._onScriptLoaded( observer ) );
 
     } );
   }
 
-  private startScriptLoad() {
-    window[ 'twttr' ] = (function( d, s, id, url ) {
-      let script,
-        firstScriptEl = d.getElementsByTagName( s )[ 0 ],
-        twitterScript = window[ 'twttr' ] || {};
-      if ( d.getElementById( id ) ) {
-        return twitterScript;
-      }
+  private _startScriptLoad(): void {
+    const twitterData = this._document.defaultView[ this.TWITTER_OBJECT ] ?? {};
 
-      script = d.createElement( s );
-      script.id = id;
-      script.src = url;
-      firstScriptEl.parentNode.insertBefore( script, firstScriptEl );
+    if ( this._scriptExists() ) {
+      this._document.defaultView[ this.TWITTER_OBJECT ] = twitterData;
+      return;
+    }
 
-      twitterScript._e = [];
+    this._appendScriptToDOM();
 
-      twitterScript.ready = function( f ) {
-        twitterScript._e.push( f );
-      };
+    twitterData._e = [];
 
-      return twitterScript;
-    }( document, 'script', this.TWITTER_SCRIPT_ID, this.TWITTER_WIDGET_URL ));
+    twitterData.ready = ( callback: Function ) => {
+      twitterData._e.push( callback );
+    };
+
+    this._document.defaultView[ this.TWITTER_OBJECT ] = twitterData;
+  }
+
+  private _scriptExists(): boolean {
+    const script = this._document.getElementById( this.TWITTER_SCRIPT_ID );
+    return script !== null || typeof script !== 'object';
+  }
+
+  private _appendScriptToDOM(): void {
+    const firstScriptEl = this._document.getElementsByTagName( 'script' )[ 0 ],
+      script = this._document.createElement( 'script' );
+
+    script.id = this.TWITTER_SCRIPT_ID;
+    script.src = this.TWITTER_WIDGET_URL;
+    firstScriptEl.parentNode.insertBefore( script, firstScriptEl );
+  }
+
+  private _onScriptLoaded( observer: Observer<any> ): Function {
+    return ( twitterData: any ) => {
+      observer.next( twitterData );
+      observer.complete();
+    };
   }
 
 }
